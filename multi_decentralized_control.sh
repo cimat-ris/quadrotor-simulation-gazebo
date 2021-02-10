381434e87fa1ef32160dc5c96e9a3da81d57973a
JBH <jbhayet@cimat.mx>
2021-02-10 10:48:15 -0600

    Scripts

---
+++ multi_decentralized_control.sh
@@ -0,0 +1,9 @@
+#!/bin/bash  
+
+rosrun multi_uav_control decentralized_control_node_uav_0 &
+
+rosrun multi_uav_control decentralized_control_node_uav_1 &
+
+rosrun multi_uav_control decentralized_control_node_uav_2 &
+
+rosrun multi_uav_control decentralized_control_node_uav_3 &
